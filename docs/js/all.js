c39db8417a86fc5ee56a80322d2498924953d1a9
unknown <lampreaomar@gmail.com>
2021-10-07 13:25:40 -0500

    configuración para inicializar firebase

--- docs/js/all.js
+++ docs/js/all.js
@@ -28,7 +28,31 @@ function dropMenu(){
   })
 }
 document.addEventListener('DOMContentLoaded', e => {
-
+  //animaciones menu:
   dropMenu();
+
+  //Firestore:
+  const firebaseConfig = {
+    apiKey: "AIzaSyC7WRbMMBb_w-eAOCxQRr-Dj3XTlS4Hmgs",
+    authDomain: "cleancode-8bc7d.firebaseapp.com",
+    projectId: "cleancode-8bc7d",
+    storageBucket: "cleancode-8bc7d.appspot.com",
+    messagingSenderId: "850754643948",
+    appId: "1:850754643948:web:596d8d793cb3d4c78f02c9",
+    measurementId: "G-97SC9T3N3N"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  //initialize firestore database 
+  const database = firebase.firestore()
+  //initialize auth service
+  const auth = firebase.auth()
+  const proveedor = new firebase.auth.GoogleAuthProvider()
+
+
+
+
+  //data functions....................
 
 })

c338fdf78f4d416bf1852fdadfd0aa1b18b0d33b
unknown <lampreaomar@gmail.com>
2021-10-12 11:17:30 -0500

    login

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', e => {
   //animaciones menu:
-  dropMenu();
+  if(window.location.pathname != '/') dropMenu();
 
   //Firestore:
   const firebaseConfig = {
@@ -12,14 +12,61 @@ document.addEventListener('DOMContentLoaded', e => {
     appId: "1:850754643948:web:596d8d793cb3d4c78f02c9",
     measurementId: "G-97SC9T3N3N"
   };
+  const btnLogin = document.getElementById('btn-login')
+  const btnLogout = document.getElementById('btn-logout')
   
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   //initialize firestore database 
   const database = firebase.firestore()
+
   //initialize auth service
-  const auth = firebase.auth()
-  const proveedor = new firebase.auth.GoogleAuthProvider()
+  // const auth = firebase.auth()
+  // const proveedor = new firebase.auth.GoogleAuthProvider()
+
+
+  //Funciones para login y logout
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      console.log('login');
+    } else {
+      // User is signed out
+      console.log('logout');
+    }
+  });
+
+  if(window.location.pathname === '/'){
+    btnLogin.addEventListener('click', e =>{
+      const email = document.getElementById('email').value
+      const password = document.getElementById('pwd').value
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('inicio de sesión exitoso =)', user.email);
+        // window.location.pathname = '/dashboard.html'
+        localStorage.setItem('user', user.email)
+        localStorage.getItem('user') ? window.location.pathname = '/dashboard.html' : window.location.pathname = '/'
+      })
+      .catch((error) => {
+        console.log('error al iniciar sesión', error.message);
+        alert('usuario o contraseña invalida')
+      });
+    })
+  }
+  
+
+  btnLogout.addEventListener('click', e =>{
+    firebase.auth().signOut().then(() => {
+      console.log('cierre de sesión exitoso');
+      localStorage.clear()
+    }).catch((error) => {
+      console.log('cierre de sesión fallido', error);
+    });
+  })
 
 
 
